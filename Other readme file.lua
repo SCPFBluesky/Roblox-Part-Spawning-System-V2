8218542fdd90eaf318cb10b902d2f3c7e44e28bd
SCPFBluesky <144206470+SCPFBluesky@users.noreply.github.com>
2024-08-02 15:33:19 -0500

    Create Other readme file.lua

---
+++ Other readme file.lua
@@ -0,0 +1,10 @@
+--[[
+	If you have not teams at all you can delete the example team folders and
+	keep the netrual spawnpoint, rename it to "Part"
+	Make as many folders for your teams, this also supports mutiple spawn locations for one team.
+	MAKE SURE WHEN YOU SET UP A SPAWN, MAKE SURE YOU MAKE THE SPAWN CANCOLIDE OFF
+	AND A LITTLE OFF THE GROUND ELSE YOU COULD FACE SOME PROBLEMS.
+
+	If you would like to make your own changes to this system please vist the github:
+	https://github.com/SCPFBluesky/Roblox-Part-Spawning-System-V2
+]]
